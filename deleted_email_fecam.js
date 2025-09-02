294d57731630c4d3285e2a83b1ba7185a8b85d14
FishingDonut <protronsdimi@gmail.com>
2025-09-02 10:36:20 -0300

    update

--- deleted_email_fecam.js
+++ deleted_email_fecam.js
@@ -1,3 +1,6 @@
+// javascript:(function(){function c(e){const b={bubbles:true,cancelable:true,view:window,ctrlKey:true,metaKey:true,button:0,buttons:1,detail:1};if(window.PointerEvent){e.dispatchEvent(new PointerEvent("pointerdown",{...b,pointerId:1,pointerType:"mouse",isPrimary:true}))}e.dispatchEvent(new MouseEvent("mousedown",b));e.dispatchEvent(new MouseEvent("mouseup",b));e.dispatchEvent(new MouseEvent("click",b))}const msgs=document.querySelectorAll(".message");const excluir=document.querySelector(".delete");msgs.forEach(m=>{m.classList.add("selected");const t=m.querySelector("a,button,[role='button'],[data-click-target],.click-target")||m;c(t)});excluir&&excluir.click();})();
+
+
 function ctrlMetaClick(el) {
   const base = {
     bubbles: true,
